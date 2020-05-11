a2713fcf04898b4e5c927d3e466b14fdcaabbc2b
tshflyer <tshflyer@163.com>
2020-05-11 21:28:04 +0800

    开启gzip优化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+
 const path = require('path')
 function resolve(dir) {
 	return path.join(__dirname, dir)
@@ -24,7 +26,14 @@ module.exports = {
 			alias: {
 				'@': resolve('src')
 			}
-		}
+		},
+		plugins: [
+			new CompressionPlugin({
+				test: /\.js$|\.html$|\.css$|\.jpg$|\.jpeg$|\.png/, // 需要压缩的文件类型
+				threshold: 10240, // 归档需要进行压缩的文件大小最小值，我这个是10K以上的进行压缩
+				deleteOriginalAssets: false // 是否删除原文件
+			})
+		]
 	},
 	css: {
 		loaderOptions: {
