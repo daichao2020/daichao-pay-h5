83280215a46d3286eca7a93c6917d44cbe25f799
tshflyer <tshflyer@163.com>
2020-06-23 23:16:27 +0800

    new Rupee 打包

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,8 @@ module.exports = {
 		proxy: {
 			'/api': {
 				//target: 'http://idc.wonderloan.top/api',
-				target: 'http://idc.daichaochina.top/api',
-				//target: 'http://newrupee.daichaochina.top/api',
+				//target: 'http://idc.daichaochina.top/api',
+				target: 'http://newrupee.daichaochina.top/api',
 				ws: false,//是否代理websockets
 				changeOrigin: true,   // 设置同源  默认false，是否需要改变原始主机头为目标URL
 				pathRewrite:{
