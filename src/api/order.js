f3af69163a9c52f1574c1d5ed15e958fd27618ee
tshflyer@163.com <tshflyer@163.com>
2020-07-03 21:53:27 +0800

    Razor pay

--- src/api/order.js
+++ src/api/order.js
@@ -28,7 +28,13 @@ export function submitOrdersCashfree(data) {//会员生成订单
 		data
 	})
 }
-
+export function submitOrdersRazorpay(data) {//会员生成订单
+	return request({
+		url: '/member/orders/razorpay',
+		method: 'post',
+		data
+	})
+}
 export function payOrders(data) {//会员订单支付
 	return request({
 		url: `/orders/${data.order_id}/mpurse`,
@@ -39,7 +45,7 @@ export function payOrders(data) {//会员订单支付
 
 export function queryOrderPayStatus(order_id) {//会员订单查询
 	return request({
-		url: '/payment/cashfree/return',
+		url: '/payment/razorpay/return',
 		method: 'get',
 		params: { order_id }
 	})
--- src/components/Public/VipSwiper.vue
+++ src/components/Public/VipSwiper.vue
@@ -72,7 +72,7 @@
 
 </template>
 <script>
-	import { getMemberCardList,submitOrdersCashfree,payOrders } from '@/api/order';
+	import { getMemberCardList,submitOrdersRazorpay,payOrders } from '@/api/order';
 	import { Toast } from 'vant';
 
 	export default {
@@ -213,7 +213,7 @@
 				}
 
 
-				submitOrdersCashfree(params).then((res)=>{
+				submitOrdersRazorpay(params).then((res)=>{
 					const { data } = res;
 					this.isSubmitting = false;
 					if(data){
