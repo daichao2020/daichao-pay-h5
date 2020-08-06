03a1acb86a996e5504d1854cbfabd5cbe9a63fa0
tshflyer@163.com <tshflyer@163.com>
2020-08-06 20:26:00 +0800

    更新隐私协议~

--- src/components/Policy/index.vue
+++ src/components/Policy/index.vue
@@ -13,116 +13,307 @@
 
 			<div class=WordSection1 style='layout-grid:15.6pt'>
 
-				<p class=MsoNormal><span lang=EN-US >Privacy
-Policy</span></p>
+				<p class=MsoNormal><span lang=EN-US>Privacy policy</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;the
-myloan app as a Free app. This SERVICE is provided&nbsp; at no cost and is
-intended for use as is.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Smallloan is a free app . the service is
+provided at no cost</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >This
-page is used to inform visitors regarding my policies with the collection, use,
-and disclosure of Personal Information if anyone decided to use my Service.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>How do we collect your information</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >If
-you choose to use my Service, then you agree to the collection and use of
-information in relation to this policy. The Personal Information that I collect
-is used for providing and improving the Service. I will not use or share your
-information with anyone except as described in this Privacy Policy.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>We will obtain your personal information
+through the following four channels:</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >The
-terms used in this Privacy Policy have the same meanings as in our Terms and
-Conditions, which is accessible at myloan unless otherwise defined in this
-Privacy Policy.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>1. Information you provide directly: we may
+collect and store any information you provide us when using smallloan financial
+services, including when you fill in information online on our page</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >Information
-Collection and Use</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>2. Information obtained from other sources:
+with your consent, we may also obtain your relevant information from smallloan
+financial affiliated companies and partners, credit institutions and
+third-party institutions legally established and legally retaining your
+relevant information. We may obtain the account information you are authorized
+to share from a third party and log in directly through the third party account
+and use our products or services after you agree to this privacy policy. We
+will use your personal information in accordance with the agreement with a
+third party and after confirming the legality of the source of personal
+information, and on the premise of complying with relevant laws and
+regulations. With your consent, we may obtain your personal information from a
+third party that legally holds your personal information.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >myloan
-verifies your identity and assesses your credits by collecting your full legal
-name, national ID, Mobile number (Submitted Information). No loan can be
-disbursed if the data is missing. Note that we take your privacy seriously. All
-your data is well encrypted and secured. We promise that we never share or sell
-your information with third parties without your permission.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>3. Information inquired, printed and retained
+from administrative and judicial organs;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >Security</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>4. Other information obtained from legal
+sources.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >I
-value your trust in providing us your Personal Information, thus we are
-striving to use commercially acceptable means of protecting it. But remember
-that no method of transmission over the internet, or method of electronic
-storage is 100% secure and reliable, and I cannot guarantee its absolute
-security.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span style='font-family:宋体'>（</span><span lang=EN-US>3</span><span
+						style='font-family:宋体'>）</span><span lang=EN-US> How do we use your information</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >Links
-to Other Sites</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Our main purpose of collecting your
+information is to provide you with a safe, efficient and personalized service
+experience. We will use your personal information for the following purposes:</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >This
-Service may contain links to other sites. If you click on a third-party link,
-you will be directed to that site. Note that these external sites are not
-operated by me. Therefore, I strongly advise you to review the Privacy Policy
-of these websites. I have no control over and assume no responsibility for the
-content, privacy policies, or practices of any third-party sites or services.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>1. Conduct qualification, credit and
+solvency reviews</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >Children</span><span
-				>’<span lang=EN-US>s Privacy</span></span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>When you use smallloan financial services,
+we will use your basic personal information, personal identity information,
+personal property information and other information collected based on your
+consent or obtained from a third party in the process of specific business
+development to evaluate your credit status and conduct risk management and
+control. At the same time, detect, prevent or repair fraud or other potential
+illegal activities. At the same time, in the process of enjoying smallloan
+financial services, we will continuously use the above information to detect,
+prevent or correct the behavior of violating relevant laws and regulations or
+applicable user agreements.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >These
-Services do not address anyone under the age of 13. I do not knowingly collect
-personally identifiable information from children under 13. In the case I
-discover that a child under 13 has provided me with personal information, I
-immediately delete this from our servers. If you are a parent or guardian and
-you are aware that your child has provided us with personal information, please
-contact me so that I will be able to do necessary actions.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>2. To provide you with your choice of
+smallloan financial related services</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >Changes
-to This Privacy Policy</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>When you use smallloan financial related
+services, such as loan, financial management, payment and insurance services,
+we will use your personal identity information, personal property information
+and other information collected based on your consent in the specific business
+process to evaluate the applicability of our products or services, verify the
+authenticity of transactions, process product or service requests, and complete
+transaction instructions Order and send you transaction notice, etc. It will
+also be used to compare the accuracy of the information and verify with a third
+party. At the same time, in order to comply with the relevant laws and
+regulations and regulatory provisions, as well as to facilitate you to query
+transaction records or historical status, we will archive your identity
+information, transaction information and behavior information when using
+smallloan finance, and keep these information in strict accordance with the
+provisions of laws and regulations.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >I
-may update our Privacy Policy from time to time. Thus, you are advised to
-review this page periodically for any changes. I will notify you of any changes
-by posting the new Privacy Policy on this page. These changes are effective
-immediately after they are posted on this page.</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>3. Provide customer service and handle
+complaints</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >Contact
-Us</span></p>
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Our customer service system will use your
+account information and transaction information. To ensure the security of your
+account, our call center customer service and online customer service will use
+your account information to verify your identity with you. When you need us to
+provide customer service and after-sales service related to your transaction
+information, we will query your transaction information.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US >If
-you have any questions or suggestions about my Privacy Policy, do not hesitate
-to contact me at Appengineer101@gmail.com.</span></p>
+				<p class=MsoNormal><span lang=EN-US>In order to ensure the legitimate rights
+and interests of you and others, if you are complained or complained about by
+others, we will provide your name, ID card number and complaint related content
+to the consumer protection department and the regulatory authority, so as to
+solve the complaints and disputes in a timely manner, except those prohibited
+by laws and regulations. &nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span style='font-family:宋体'>（</span><span lang=EN-US>3</span><span
+						style='font-family:宋体'>）</span><span lang=EN-US> You are fully aware that under
+the following circumstances, we do not need to obtain your authorization and
+consent to collect and use personal information:</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>1. Related to national security and
+national defense security;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>2. Related to public safety, public health
+and major public interests;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>3. Related to criminal investigation,
+prosecution, trial and execution of judgments;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>4. In order to protect the life, property
+and other major legitimate rights and interests of personal information subject
+or other individuals, but it is difficult to obtain my consent;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>5. The collected personal information is
+disclosed to the public by the subject of personal information;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>6. Your personal information collected from
+legally disclosed information, such as legal news reports, government
+information disclosure and other channels;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>7. It is necessary to sign the contract
+according to your requirements;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>8. Necessary for legal news reporting;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>9. When it is necessary for an academic
+research institution to carry out statistical or academic research based on
+public interest and provide the results of academic research or description to
+the public, the personal information contained in the results is de identified;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>10. Other circumstances stipulated by laws
+and regulations.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>(4) Rules for the use of your personal
+information</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>1. We will use the collected personal
+information in accordance with the provisions of this Privacy Policy and in
+order to realize the functions of our products or services.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>2. Please note that all personal
+information provided by you when using our products or services will continue
+to be authorized for use during your use of our products or services, unless
+you delete or reject our collection through system settings. When you cancel
+your account, we will stop using it and delete your personal information or
+anonymize it according to the requirements of relevant laws and regulations.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>3. We will make statistics on the use of
+our products or services and may share these statistics with the public or
+third parties to show the overall usage trend of our products or services.
+However, these statistics do not contain any of your identification
+information.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>4. We will seek your prior consent when
+using your personal information for other purposes not specified in this
+policy, or for other purposes that are collected for a specific purpose.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>IV. safety protection measures</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>1. Our technology and measures to protect
+your personal information</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>We attach great importance to personal
+information security and take all reasonable and feasible measures to protect
+your personal information:</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>We will adopt the security protection
+measures in line with the industry standards and the security technology
+prevailing in the industry to prevent your personal information from being
+accessed and modified without authorization, so as to avoid the leakage, damage
+or loss of your personal information</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>We manage and standardize the storage and
+use of personal information by establishing data classification and
+classification, data security management standards, and security development
+standards</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>(1) We use strict data access rights and
+multiple identity authentication technology to control and protect personal
+information, and through signing strict confidentiality agreement with
+information contacts, monitoring and auditing mechanism to conduct
+comprehensive security control on data, so as to avoid illegal use of data.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>(2) We use code security check and data
+access log analysis technology to audit personal information security.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>(3) We have established an information
+security committee and set up an information security group under which the
+information security specialists of each group are responsible for personal
+information security affairs.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>(4) We will also hold security and privacy
+protection training courses to enhance employees' awareness of the importance of
+protecting personal information.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>2. Security incident handling</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>(1) In order to deal with the possible
+risks of personal information leakage, damage and loss, we have formulated a
+number of systems to clarify the classification and classification standards of
+security incidents and security vulnerabilities and the corresponding
+processing procedures. We have set up a special emergency response team for
+security incidents. According to the requirements of safety incident handling
+regulations, we start safety plans for different security incidents, carry out
+stop loss, analyze, locate, formulate remedial measures, and cooperate with
+relevant departments to trace and crack down.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+
+				<p class=MsoNormal style='margin-left:0cm;text-indent:0cm'><span lang=EN-US>(2)
+</span><span lang=EN-US>Once a personal information security incident occurs,
+we will promptly inform you of the basic information and possible impact of the
+security incident, the disposal measures we have taken or will take, the
+suggestions that you can independently prevent and reduce the risk, and the
+remedial measures for you, according to the requirements of laws and
+regulations. At the same time, we will timely inform you of the relevant
+information by email, letter, telephone, push notification, etc. when it is
+difficult to inform the personal information subject one by one, we will take a
+reasonable and effective way to release an announcement. At the same time, we
+will take the initiative to report the incident to the supervision department
+according to the requirements of our supervision department.</span></p>
+
+				<p class=MsoNormal><span lang=EN-US>Firebone300@gmail.com</span></p>
 
 			</div>
-
 
 		</section>
 	</div>
