548e0154811cf90f48522b37dc6af28492c371c0
tshflyer@163.com <tshflyer@163.com>
2020-08-13 19:25:29 +0800

    【online loan 全功能版】修改【隐私协议】

--- src/components/Login/terms.vue
+++ src/components/Login/terms.vue
@@ -10,262 +10,127 @@
 			></van-nav-bar>
 		</header>
 		<section class="page-body">
+
 			<div class=WordSection1 style='layout-grid:15.6pt'>
 
-				<p class=MsoNormal><span lang=EN-US>***Customer Privacy Agreement</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> In
-order to effectively protect the privacy of *** customers, *** has formulated
-this &quot;*** Customer Privacy Protection Agreement&quot; (hereinafter
-referred to as &quot;this agreement&quot;) in accordance with current laws and
-policies. This agreement applies to all services provided by *** to you,
-whether you obtain *** services through a mobile terminal or other equipment.</span></p>
+				<p class=MsoNormal><span lang=EN-US>Privacy Policy</span></p>
 
 				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US> Your
-use of *** means that you know, understand and agree to accept all the contents
-of this agreement, and confirm that you bear all the legal consequences arising
-therefrom. During the reading process, if you have any questions, you can
-contact our customer service for consultation. If you do not agree to any terms
-in the agreement, you should stop using *** mobile banking immediately.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>You
-can download the *** client through a legally authorized third party, do not
-download and use client software from unknown sources. If you obtain an
-installation program titled &quot;***&quot; or a name similar to *** from a
-third party that is not legally authorized by ***, *** cannot guarantee that
-the software can be used normally, and the resulting consequences are You bear
-it yourself.</span></p>
+				<p class=MsoNormal><span lang=EN-US>The online loan application is a free
+application. This service is provided free of charge and is intended to be used
+as is.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>If someone decides to use my service, this
+page is used to inform visitors of my policy through the way we collect, use
+and disclose personal information.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>Information acquisition</span></p>
+				<p class=MsoNormal><span lang=EN-US>If you choose to use my service, you agree
+to the collection and use of information related to this policy. The personal
+information I collect is used to provide and improve services. Except as
+described in this privacy policy, I will not use or share your information with
+anyone.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Unless otherwise defined in this privacy
+policy, the terms used in this privacy policy have the same meaning as our
+terms and conditions and can be accessed through the &quot;Instant Loan
+Guide&quot;.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>When you register or use this service, you
-need to provide some necessary information, including but not limited to
-providing you with account registration services or user identification, you
-need to fill in your mobile phone number; branch exclusive wealth management
-requires your geographic location Location information; mobile phone number
-transfer requires your authorization to access the mobile phone address book;
-scheduled transfer requires your authorization to access the calendar; dialing
-an outlet or customer service call requires your authorization to use the mobile
-phone; face verification requires your authorization to access the camera and
-recording permissions; payment functions You need to authorize the use of
-mobile phone NFC; some function guides need to identify the content of your
-mobile phone clipboard, etc.; and businesses that require you to provide real
-identity information with special provisions in national laws, regulations or
-policies. If the information you provide is incomplete or fails to provide
-relevant information as required, you will not be able to use the relevant
-services or be restricted in the course of use.</span></p>
+				<p class=MsoNormal><span lang=EN-US>Information collection and use</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>In order to get a better experience, when
+using our services, I may ask you to provide us with certain personally
+identifiable information. The information I request will remain on your device,
+and I will not collect it in any way.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>In order to protect the security of your
-transactions and prevent your account from being illegally infringed by others,
-you authorize the collection, transmission, analysis, storage and use of
-personal information such as your name, credentials, bank card number, and
-mobile phone number during the use of mobile banking services. As well as the
-device model, text messages, number of call records, number of application
-lists, operating system, unique device identification number, access network
-method and type, IP address and other information on the device, and report to
-legally established telecom operators and other institutions (including but It
-is not limited to China Telecom, China Mobile, China Unicom and other external
-cooperation agencies) to inquire, use or provide, including but not limited to
-your mobile phone number, geographic location, behavior, equipment and other
-related information.</span></p>
+				<p class=MsoNormal><span lang=EN-US>The application does use third-party
+services, which may collect information to identify you.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Link to the privacy policy of the
+third-party service provider used by the application</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>Information use and management</span></p>
+				<p class=MsoNormal><span lang=EN-US>Google Play Services</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Record data</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>In order
-to provide you with better services or products, it will use your user
-information in the following situations:</span></p>
+				<p class=MsoNormal><span lang=EN-US>I want to inform you that whenever you use
+my service, if there is an error in the application, I will collect data and
+information on your phone through a third-party product (called third-party
+data). The log data may contain information such as the device's Internet
+Protocol (&quot;IP&quot;) address, device name, operating system version,
+application configuration when using &quot;My Service&quot;, the time and date
+you use the service, and other statistical information. .</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Cookies</span></p>
 
-				<p class=MsoNormal><span lang=EN-US> 1.
-According to the requirements of relevant laws and regulations;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Cookies are files with a small amount of
+data, usually used as anonymous unique identifiers. This information will be
+sent to the browser from the website you visit and stored in the device's
+memory.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>This service does not explicitly use these
+&quot;cookies&quot;. However, the application may use third-party code and
+libraries that use &quot;cookies&quot; to collect information and improve its
+services. You can choose to accept or reject these cookies and know when to
+send the cookie to your device. If you choose to reject our cookies, you may
+not be able to use some parts of this service.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>2.
-According to your authorization;</span></p>
+				<p class=MsoNormal><span lang=EN-US>service provider</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>I may hire third-party companies and
+individuals for the following reasons:</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>3.
-According to the relevant terms of service and application license agreement.</span></p>
+				<p class=MsoNormal><span lang=EN-US>Promote our services;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Provide services on our behalf;</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>In addition, you know and agree that within
-the scope permitted by current laws and regulations, your non-private user
-information may be used for marketing, including but not limited to: displaying
-or providing advertisements and promotional materials to you on the platform ,
-Service or product information notified or recommended to you, and other such
-information that may be of interest to you based on your use of the service or
-product. It also includes information that you choose to share when you take an
-action such as authorization.</span></p>
+				<p class=MsoNormal><span lang=EN-US>Perform service-related services; or</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Help us analyze how to use our services.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US> will
-not disclose your user information to any third party without your permission,
-except in the following circumstances:</span></p>
+				<p class=MsoNormal><span lang=EN-US>I want to inform users of this service that
+these third parties have the right to access your personal information. The
+reason is to perform the tasks assigned to them on our behalf. However, they
+are obliged not to disclose or use this information for any other purpose.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Safety</span></p>
 
-				<p class=MsoNormal><span lang=EN-US> 1.
-The authorization of you or your guardian has been obtained;</span></p>
+				<p class=MsoNormal><span lang=EN-US>I value your trust in providing us with
+your personal information, so we are working hard to use commercially
+acceptable methods to protect it. But please keep in mind that neither the
+transmission method nor the electronic storage method on the Internet is 100%
+safe and reliable, and I cannot guarantee its absolute security.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Link to other websites</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>2.
-Judicial or administrative agencies require disclosure according to legal
-procedures;</span></p>
+				<p class=MsoNormal><span lang=EN-US>The service may contain links to other
+sites. If you click on a third-party link, you will be directed to that site.
+Please note that these external sites are not operated by me. Therefore, I
+strongly recommend that you read the privacy policies of these websites. I have
+no control over the content, privacy policy or behavior of any third-party site
+or service, and I am not responsible for it.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Child privacy</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>3.
-When litigation or arbitration is filed against the user in order to protect
-its legal rights and interests;</span></p>
+				<p class=MsoNormal><span lang=EN-US>These services are not for anyone under the
+age of 13. I will not knowingly collect personally identifiable information
+from children under 13 years of age. If I find that a child under the age of 13
+has provided me with personal information, I will immediately delete this information
+from our servers. If you are a parent or guardian and you know that your child
+has provided us with personal information, please contact me so that I can take
+the necessary measures.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>Changes to this privacy policy</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>4.
-According to the agreement between you and the relevant service terms and
-application license agreement;</span></p>
+				<p class=MsoNormal><span lang=EN-US>I may update our privacy policy from time
+to time. Therefore, it is recommended that you check this page regularly to
+make any changes. I will notify you of any changes by posting a new privacy
+policy on this page.</span></p>
 
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
+				<p class=MsoNormal><span lang=EN-US>contact us</span></p>
 
-				<p class=MsoNormal><span lang=EN-US> 5.
-Other circumstances stipulated by laws and regulations.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>Information protection</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> will
-make all reasonable efforts to protect the user information it obtains. In
-order to prevent user information from being illegally accessed, copied,
-modified, transmitted, lost, destroyed, processed or used in unexpected and
-unauthorized circumstances, the following measures have been and will continue
-to be taken to protect your user information:</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> 1.
-Encrypt the user information of the user in an appropriate manner;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>2.
-Use passwords to protect user information in appropriate locations;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> 3.
-Restrict access to user information;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> 4.
-Other reasonable measures.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> Although
-the above reasonable and effective measures have been taken, and the standards
-required by relevant laws and regulations have been complied with, the security
-of your user information when communicating through unsafe channels cannot be
-guaranteed. Therefore, individual users should take active measures to ensure
-the security of user information, such as: regularly changing mobile banking
-passwords, and not revealing user information such as their mobile phone
-numbers and login passwords to others.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>The user information protection measures
-provided are only applicable to the platform. Once you leave, browse or use
-other websites, services and content resources, you have no ability and
-obligation to protect any user information you submit on other websites,
-regardless of whether you log in or browse the aforementioned websites Whether
-it is based on links or guides.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>Information access and revision</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>
-After you complete the mobile banking registration, you can view or modify the
-user information you submitted. In general, you can browse and modify the
-information you submit at any time, but for security and identification (such
-as number appeal service) considerations, you may not be able to modify some of
-the initial registration information and verification information provided
-during registration.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>Changes to privacy protection policy</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> has
-the right to modify any clause of this agreement at any time. Once the content
-of this agreement changes, it will be publicized, and the publicity period is
-30 natural days. After the publicity period ends, the revised agreement will be
-published directly in the mobile banking, and the publicity shall be deemed to
-have notified you of the revised content. The user may also be prompted to
-modify the content through other appropriate methods. If you do not agree to
-the changes made to the relevant terms of this agreement, you have the right to
-stop using the mobile banking service. If you continue to use the mobile
-banking service, it is deemed that you have accepted the amendments made to the
-relevant terms of this agreement.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>other</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> If
-there is a dispute between you and you, it will be resolved by both parties
-through negotiation. If the negotiation fails to file a lawsuit, the court at
-the head office will have jurisdiction.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US> The
-establishment, entry into force, performance, interpretation and dispute
-resolution of this agreement shall be governed by the laws of the mainland of
-the People</span><span style='font-family:宋体'>’</span><span lang=EN-US>s
-Republic of China. If there is no express provision in the law, the prevailing
-domestic financial practices may apply.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>The
-terms of this agreement are partially invalid or unenforceable for any reason,
-and will not affect the validity of other terms of this agreement.</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>&nbsp;</span></p>
-
-				<p class=MsoNormal><span lang=EN-US>
-Matters not covered in this agreement shall be handled in accordance with laws
-and regulations, relevant business rules and domestic prevailing financial
-practices.</span></p>
+				<p class=MsoNormal><span lang=EN-US>If you have any questions or suggestions
+about my privacy policy, please contact me at Xingbar786@gmail.com</span></p>
 
 			</div>
 
