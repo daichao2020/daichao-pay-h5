245fa36772dd63791fd864a84b6730db17b87955
tshflyer@163.com <tshflyer@163.com>
2020-08-16 01:39:24 +0800

    【Rupee Hub 全功能版】优化修改~

--- src/main.js
+++ src/main.js
@@ -6,14 +6,6 @@ import 'amfe-flexible'
 import 'vant/lib/icon/local.css';
 import '@/assets/styles/index.scss';
 
-/**缓动 */
-import tween from "./plugins/tween";
-Vue.use(tween);
-
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-// import style
-import 'swiper/css/swiper.css'
-Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 import '@/permission' // permission control
 
 import {
