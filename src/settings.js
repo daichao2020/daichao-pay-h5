26d698330746371e0fad57279ef3ec1f1f94ea36
tshflyer@163.com <tshflyer@163.com>
2020-09-02 21:07:51 +0800

    OnlineLoan 普通贷超版

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-	title				: 'Cash Wallet',
+	title				: 'Online Loan',
 	deviceNumber		: '5566777',//设备号
 	platform			: '3',//平台：1-安卓 2-苹果 3-H5
-	appVersionId		: '10',//App 版本 id
+	appVersionId		: '43',//App 版本 id
 
 }
