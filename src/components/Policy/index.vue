53df016338185151c3ca4f6f92dd20694d8c1d0f
tshflyer@163.com <tshflyer@163.com>
2020-10-10 20:24:35 +0800

    修改api地址+隐私协议

---
+++ src/components/Policy/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="default-page policy-page">
+
+		<header v-if="isShowHeader">
+			<van-nav-bar
+					title="Privacy policy"
+					fixed
+					placeholder
+			></van-nav-bar>
+		</header>
+
+		<section class="page-body">
+			<div class="WordSection1"><p class="MsoNormal"><span>Data Privacy Policy</span></p><p class="MsoNormal"><span>Please read this Privacy Policy carefully before accessing or using the Service. This Privacy Policy contains important terms that affect your legal rights. It is understood that upon submission of the application form, you agree to be bound by the terms and conditions set forth below and automatically consent to the collection, use, storage, analysis and manipulation of your data information in accordance with this Privacy Policy to provide you with a great customer experience. If you do not wish to be bound by these terms and conditions, you may not access or use money loan Service.</span></p><p class="MsoNormal"><span>Please note that this privacy policy may be amended from time to time to comply with Data Privacy requirements, coverage and technology security advancements and such modifications shall be effective immediately upon posting of the modified Agreement.</span></p><p class="MsoNormal"><span>Terms and Definitions</span></p><p class="MsoNormal"><span>Consent </span><span>–</span><span> Any freely given, specific, informed indication of will for the collection and processing of his or her personal, sensitive and privileged information.</span></p><p class="MsoNormal"><span>Company </span><span>–</span><span> refers to </span><span>“</span><span lang="EN-US">money loan</span><span>”</span><span lang="EN-US">.</span></p><p class="MsoNormal"><span>Data Outsourcing </span><span>–</span><span> disclosure or transfer of personal data by a personal information controller to a personal information processor.</span></p><p class="MsoNormal"><span>Data Sharing </span><span>–</span><span> Disclosure or transfer to a third party of personal data under the custody of a personal information controller or personal information processor. Disclosure or transfer of the personal information processor must be upon the instructions of the personal information controller.</span></p><p class="MsoNormal"><span>Data Subject - Refers to an individual whose personal, sensitive, and/or privileged information is processed.</span></p><p class="MsoNormal"><span>Personal Information </span><span>–</span><span> refers to all types of personal information which are apparent or can be reasonably and directly ascertained by the entity holding the information such as:</span></p><p class="MsoNormal"><span></span><span>Full Name;</span></p><p class="MsoNormal"><span></span><span>Photo;</span></p><p class="MsoNormal"><span></span><span>Signature;</span></p><p class="MsoNormal"><span></span><span>IP Address;</span></p><p class="MsoNormal"><span></span><span>SMS</span></p><p class="MsoNormal"><span></span><span>Contact</span></p><p class="MsoNormal"><span></span><span>Postpaid Number; </span></p><p class="MsoNormal"><span></span><span>Biometrics;</span></p><p class="MsoNormal"><span>Personal Information Controller (PIC) </span><span>–</span><span> refers to a natural or juridical person, or any other body who controls the processing of personal data, or instructs another to process personal data on its behalf</span></p><p class="MsoNormal"><span>Personal Information Processor (PIP) </span><span>–</span><span> refers to any natural or juridical person, or any other body who controls the processing of personal data, or instructs another to process personal data on its behalf</span></p><p class="MsoNormal"><span>Processing </span><span>–</span><span> refers to any operation or any set of operations performed upon personal data including, but not limited to collection, recording, organization, storage, updating or modification, retrieval, consultation, use, consolidation, blocking, erasure or destruction of data. Processing may be performed through automated means, or manual processing, if the personal data are contained or are intended to be contained in a filing system.</span></p><p class="MsoNormal"><span>Sensitive Personal Information </span><span>–</span><span> refers to personal information about the data subjects:</span></p><p class="MsoNormal"><span></span><span>Race;</span></p><p class="MsoNormal"><span></span><span>Ethnic Origin;</span></p><p class="MsoNormal"><span></span><span>Marital status;</span></p><p class="MsoNormal"><span></span><span>Age;</span></p><p class="MsoNormal"><span></span><span>Religious or Philosophical belief;</span></p><p class="MsoNormal"><span></span><span>Political Affiliation;</span></p><p class="MsoNormal"><span></span><span>Education;</span></p><p class="MsoNormal"><span></span><span>Genetic or sexual life;</span></p><p class="MsoNormal"><span></span><span>Any offense involving such person;</span></p><p class="MsoNormal"><span></span><span>Social Security Numbers;</span></p><p class="MsoNormal"><span></span><span>Licenses;</span></p><p class="MsoNormal"><span></span><span>Specifically Established by law to be kept classified</span></p><p class="MsoNormal"><span>Your Consent</span></p><p class="MsoNormal"><span>You hereby give your acknowledgement and consent to money loan to use your personal data for the aforesaid Purposes and Services. You agree that your consent will remain in place until your withdrawal by officially notifying the Company in writing.</span></p><p class="MsoNormal"><span>Personal Information and Sensitive Personal Information</span></p><p class="MsoNormal"><span>Personal and Sensitive Personal information are information that can be used to uniquely identify a user such as name, password, date of birth, civil status, gender, email address, telephone number, and other personal details. Below is a list of information that may be collected:</span></p><p class="MsoNormal"><span>•</span><span> Your SSS and/or TIN number;</span></p><p class="MsoNormal"><span>•</span><span> Your employment and income information;</span></p><p class="MsoNormal"><span>•</span><span> Your bank account information;</span></p><p class="MsoNormal"><span>•</span><span> Any personal information you provide us in connection with any customer service communication (email, phone or other means), dispute or complaint.</span></p><p class="MsoNormal"><span>You may be asked to provide Personal Information and/or Sensitive Personal Information to meet our legal obligations, verify your identity, determine eligibility for credit, protect against fraud, and complete your transaction.</span></p><p class="MsoNormal"><span>We may also use the provided Personal Information and Sensitive Personal Information to inform you of special loan offers, to process your loans, to provide you with our services or to conduct surveys. While the Company collects these information on a voluntary basis for certain products and services, The Company</span><span>’</span><span lang="EN-US">s collection of personally identifiable information may be a requirement for access to our services or to process a user's loan.</span></p><p class="MsoNormal"><span>How To Contact Us</span></p><p class="MsoNormal"><span>For any queries, clarifications or requests on any aspect of this Statement, the exercise of your rights pertaining to your personal information or to provide any feedback that you may have about our processing of personal information, you may get in touch with our Data Privacy Officer at:</span></p><p class="MsoNormal"><span>The Data Protection Officer</span></p><p class="MsoNormal"><span><a href="mailto:couplecartoon009@gmail.com">couplecartoon009@gmail.com</a></span></p><p class="MsoNormal"><span>Changes to Our Privacy Statement</span></p><p class="MsoNormal"><span>We may modify or amend this Privacy Statement from time to time to keep up with any changes in relevant laws and regulations applicable to us or how we collect, use, protect, store, share or dispose of your personal information. Any relevant updates will be posted on money loan website and mobile application.</span></p></div>
+		</section>
+	</div>
+</template>
+<script>
+
+	export default {
+		data(){
+			return {
+
+			}
+		},
+		mounted(){
+
+		},
+		computed: {
+			isShowHeader() {
+				const route = this.$route
+				const { meta } = route
+				return meta.showHeader
+			},
+		},
+		methods: {
+			onClickLeft() {
+				this.$router.go(-1);
+			},
+		}
+	}
+</script>
+<style lang="scss">
+	.policy-page{
+		.page-body {
+			padding: .4rem;
+		}
+		p.MsoNormal, li.MsoNormal, div.MsoNormal
+		{margin:0cm;
+			margin-bottom:.0001pt;
+			text-justify:inter-ideograph;
+			font-size:10.5pt;
+			font-family:"Calibri",sans-serif;}
+		/* Page Definitions */
+		@page WordSection1
+		{size:595.3pt 841.9pt;
+			margin:72.0pt 90.0pt 72.0pt 90.0pt;
+			layout-grid:15.6pt;}
+		div.WordSection1
+		{page:WordSection1;}
+	}
+</style>
