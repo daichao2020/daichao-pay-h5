7f559be1c2815ab5deea9ca565e7b3702569564d
tshflyer@163.com <tshflyer@163.com>
2020-10-15 21:53:29 +0800

    【mi rupee】 登录修改+忘记密码

--- src/permission.js
+++ src/permission.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
 
-const whiteList = ['/login','/code','/terms'] // no redirect whitelist
+const whiteList = ['/login','/code','/terms','/resetpassword'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
 
