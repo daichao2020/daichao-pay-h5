d87cf8ff9962554eec3b31166aa332c5b4639fb3
tshflyer@163.com <tshflyer@163.com>
2020-10-15 21:30:51 +0800

    【cashView】 登录修改+忘记密码

--- src/router/index.js
+++ src/router/index.js
@@ -70,6 +70,17 @@ export const constantRoutes = [
 		},
 		component: () => import('@/components/Login/index'),
 	},
+	//重设密码
+	{
+		path: '/resetpassword',
+		name: 'resetPassword',
+		meta: {
+			title: 'Reset Password',
+			showHeader: true,
+			keepAlive: false
+		},
+		component: () => import('@/components/Login/resetPassword'),
+	},
 	//获取验证码
 	{
 		path: '/code',
