159d044c1faecb056829baec25c72096a0c63a20
tshflyer@163.com <tshflyer@163.com>
2020-10-23 21:23:51 +0800

    一系列优化： 1.增加【关于我们】 2.增加【faq】 3.增加客服,以及点击复制 4.轮播图动态图片 5.增加产品列表标签 6.产品详情页调整（日利率，下款时间，费用说明，逾期政策）

--- src/components/Login/index.vue
+++ src/components/Login/index.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="login-page default-page">
-        <header v-if="isShowHeader">
-            <van-nav-bar
-                    title="Sign in"
-                    fixed
-                    placeholder
-            ></van-nav-bar>
-        </header>
-        <section class="login-page-body">
-            <h2 class="vip-doc-block__title">Welcome</h2>
-            <div ref="login_form">
-                <van-field
-                        readonly
-                        name="picker"
-                        :value="countryCodeText"
-                        label="Country देश"
-                        placeholder="Please select country code"
-                />
-                <!--
+	<div class="login-page default-page">
+		<header v-if="isShowHeader">
+			<van-nav-bar
+					title="Sign in"
+					fixed
+					placeholder
+			></van-nav-bar>
+		</header>
+		<section class="login-page-body">
+			<h2 class="vip-doc-block__title">One stop loan platform in India</h2>
+			<div ref="login_form">
+				<!--<van-field
+						readonly
+						name="picker"
+						:value="countryCodeText"
+						label="Country देश"
+						placeholder="Please select country code"
+				/>-->
+				<!--
 				<van-popup v-model="showPicker" position="bottom">
 					<van-picker
 							show-toolbar
@@ -26,188 +26,188 @@
 							@cancel="showPicker = false"
 					/>
 				</van-popup>-->
-                <van-field
-                        v-model="telephone"
-                        name="telephone"
-                        type="tel"
-                        label="Telephone टेलीफोन"
-                        placeholder="Please enter telephone"
-                        @blur="checkIsNeedConfirmPassword"
-                        :rules="[{ validator, message: 'Only numbers allowed' }]"
-                />
-                <van-field
-                        v-model="password"
-                        name="password"
-                        type="password"
-                        label="Password कुंजिका"
-                        placeholder="Please enter password"
-                />
-                <van-field
-                        v-if="isNeedConfirmPassword"
-                        v-model="confirm_password"
-                        name="confirm_password"
-                        type="password"
-                        label="Confirm Password कुंजिका"
-                        placeholder="Please confirm password"
-                />
+				<van-field
+						v-model="telephone"
+						name="telephone"
+						type="tel"
+						label="Telephone टेलीफोन"
+						placeholder="Please enter telephone"
+						:rules="[{ validator, message: 'Only numbers allowed' }]"
+				/>
+				<van-field
+						v-model="password"
+						name="password"
+						type="password"
+						label="Password कुंजिका"
+						placeholder="Please enter password"
+				/>
+				<van-field
+						v-if="isNeedConfirmPassword"
+						v-model="confirm_password"
+						name="confirm_password"
+						type="password"
+						label="Confirm Password कुंजिका"
+						placeholder="Please confirm password"
+				/>
 
-                <div class="vip-btn-wrap">
-                    <van-button type="primary" class="vip-btn"
-                                :disabled="isSubmitting"
-                                :loading="isSubmitting"
-                                loading-text="Submitting..."
-                                @click="onSubmit"
-                                block>Login (लॉग इन करें) / Registration (पंजीकरण)</van-button>
-                    <div class="login-tips text-right">
-                        <p>Unregistered users will automatically create accounts</p>
-                        <p>अपंजीकृत उपयोगकर्ता स्वचालित रूप से खाते बनाएंगे</p>
-                    </div>
-                    <div class="login-forgot-password text-right">
-                        <a href="javascript:;" @click.prevent="toResetPasswordPage">
-                            <p>Forgot your password?</p>
-                            <p>क्या आप अपना पासवर्ड भूल गए?</p>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
+				<div class="vip-btn-wrap">
+					<van-button type="primary" class="vip-btn"
+								:disabled="isSubmitting"
+								:loading="isSubmitting"
+								loading-text="Submitting..."
+								@click="onSubmit"
+								block><p>Login to get money now</p>
+						<p>अब पैसा पाने के लिए लॉगिन करेंं</p></van-button>
+					<div class="login-tips text-right">
+						<p>Unregistered users will automatically create accounts</p>
+						<p>अपंजीकृत उपयोगकर्ता स्वचालित रूप से खाते बनाएंगे</p>
+						<br/>
+						<p>We will strictly protect your privacy, please feel free to register</p>
+						<p>हम आपकी गोपनीयता की सख्ती से रक्षा करेंगे, कृपया बेझिझक पंजीकरण करें</p>
+					</div>
+					<div class="login-forgot-password text-right">
+						<a href="javascript:;" @click.prevent="toResetPasswordPage">
+							<p>Forgot your password?</p>
+							<p>क्या आप अपना पासवर्ड भूल गए?</p>
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    </div>
+	</div>
 </template>
 <script>
-    import { getCountryCodes,getVerificationCodes,queryPwd } from '@/api/user'
-    export default {
-        data(){
-            return {
-                countryCodeText: 'India 0091',
-                countryCode: '0091',
+    import { debounce } from 'lodash'
+	import {getCountryCodes, getVerificationCodes, queryPwd} from '@/api/user'
 
-                columns: [],
-                showPicker: false,
+	export default {
+		data() {
+			return {
+				countryCodeText: 'India 0091',
+				countryCode: '0091',
 
-                telephone: '',//9829666666
-                password: '',
-                confirm_password: '',
-                isNeedConfirmPassword: true,
-                code: '',
-                codekey: '',
-                checkbox: true,
-                isGetting: false,
-                gettingText: '',
-                count: 60,
-                COUNT_DEFAULT: 60,
-                isSubmitting: false,
-            }
-        },
-        created(){
-            /*getCountryCodes().then((res)=>{
-				const list = res.data || [];
-				list.sort((item1,item2)=>{
-					if(item1.is_hot < item2.is_hot){
-						return 1;
-					}else if(item1.is_hot > item2.is_hot){
-						return -1;
-					}else{
-						return 0;
+				columns: [],
+				showPicker: false,
+
+				telephone: '',//9829666666
+				password: '',
+				confirm_password: '',
+				isQueryedNeedConfirmPassword: false,//是否查询了后台
+				isNeedConfirmPassword: false,
+				code: '',
+				codekey: '',
+				checkbox: true,
+				isGetting: false,
+				gettingText: '',
+				count: 60,
+				COUNT_DEFAULT: 60,
+				isSubmitting: false,
+			}
+		},
+		created() {
+            this.debouncedQueryIsNeedConfirmPassword = debounce(this.queryPassword, 500)
+		},
+		computed: {
+			isShowHeader() {
+				const route = this.$route
+				const {meta} = route
+				return meta.showHeader
+			},
+		},
+		watch: {
+            telephone(newTel, oldTel) {
+                this.debouncedQueryIsNeedConfirmPassword();
+			},
+			password() {
+				this.confirm_password = this.password;
+			},
+		},
+		methods: {
+			onConfirm(value) {
+
+				this.countryCode = value.intl_code;
+				this.countryCodeText = value.text;
+
+				this.showPicker = false;
+			},
+			onSubmit() {
+				if (!this.telephone) {
+					this.$toast('Please enter telephone');
+					return false;
+				}
+				if (!this.password) {
+					this.$toast('Please enter password');
+					return false;
+				}
+
+				if (this.isNeedConfirmPassword) {
+
+					if (!this.confirm_password) {
+						this.$toast('Please confirm password');
+						return false;
 					}
+
+					if (this.password != this.confirm_password) {
+						this.$toast("The password doesn't match.");
+						return false;
+					}
+				}
+
+				this.isSubmitting = true;
+				let params = {
+					device_number: this.$store.getters.deviceNumber,
+					platform: this.$store.getters.platform,
+					app_version_id: this.$store.getters.appVersionId,
+
+					phone_number: this.telephone,
+					password: this.password,
+				}
+				if (this.isNeedConfirmPassword) {
+					params.confirm_password = this.confirm_password
+				}
+
+				this.$store.dispatch('user/login', params)
+					.then(() => {
+						this.isSubmitting = false;
+						this.toHomePage();
+					})
+					.catch(() => {
+						this.isSubmitting = false
+					})
+
+			},
+			toHomePage() {
+				this.$router.push({name: 'home'});
+			},
+			// 校验函数返回 true 表示校验通过，false 表示不通过
+			validator(val) {
+				return /^[0-9]*$/.test(val);
+			},
+			checkIsNeedConfirmPassword() {
+				if (this.telephone) {
+					this.queryPassword();
+				}
+			},
+			queryPassword() {
+				return new Promise((resolve, reject) => {
+					let params = {
+						phone_number: this.telephone
+					}
+					queryPwd(params).then(res => {
+						let data = res.data;
+						this.isNeedConfirmPassword = data.has_pwd == 'Y' ? false : true;
+					});
+				})
+
+			},
+			toResetPasswordPage() {
+				this.$router.push({
+					name: 'resetPassword',
 				});
-				list.forEach((item)=>{
-					item.text = item.name +' '+ item.intl_code;
-				});
-				this.columns = list;
-			});*/
-        },
-        computed: {
-            isShowHeader() {
-                const route = this.$route
-                const { meta } = route
-                return meta.showHeader
-            },
-        },
-        methods: {
-            onConfirm(value) {
-
-                this.countryCode = value.intl_code;
-                this.countryCodeText = value.text;
-
-                this.showPicker = false;
-            },
-            onSubmit(){
-                if(!this.telephone){
-                    this.$toast('Please enter telephone');
-                    return false;
-                }
-                if(!this.password){
-                    this.$toast('Please enter password');
-                    return false;
-                }
-                if(this.isNeedConfirmPassword){
-
-                    if(!this.confirm_password){
-                        this.$toast('Please confirm password');
-                        return false;
-                    }
-
-                    if(this.password != this.confirm_password){
-                        this.$toast("The password doesn't match.");
-                        return false;
-                    }
-                }
-
-                this.isSubmitting = true;
-                let params = {
-                    device_number: this.$store.getters.deviceNumber,
-                    platform: this.$store.getters.platform,
-                    app_version_id: this.$store.getters.appVersionId,
-
-                    phone_number: this.telephone,
-                    password: this.password,
-                }
-                if(this.isNeedConfirmPassword){
-                    params.confirm_password = this.confirm_password
-                }
-
-                this.$store.dispatch('user/login', params)
-                        .then(() => {
-                            this.isSubmitting = false;
-                            this.toHomePage();
-                        })
-                        .catch(() => {
-                            this.isSubmitting = false
-                        })
-
-            },
-            toHomePage(){
-                this.$router.push({name:'home'});
-            },
-            // 校验函数返回 true 表示校验通过，false 表示不通过
-            validator(val) {
-                return /^[0-9]*$/.test(val);
-            },
-            checkIsNeedConfirmPassword(){
-                if(this.telephone){
-                    this.queryPassword();
-                }
-            },
-            queryPassword(){
-                return new Promise((resolve, reject) => {
-                    let params = {
-                        phone_number: this.telephone
-                    }
-                    queryPwd(params).then(res=>{
-                        let data = res.data;
-                        this.isNeedConfirmPassword = data.has_pwd == 'Y' ? false : true;
-                    });
-                })
-
-            },
-            toResetPasswordPage(){
-                this.$router.push({
-                    name:'resetPassword',
-                });
-            },
-        }
-    }
+			},
+		}
+	}
 </script>
 <style>
 
--- src/components/Me/index.vue
+++ src/components/Me/index.vue
@@ -19,6 +19,22 @@
 				<van-cell title="Feedback(प्रतिपुष्टि)" is-link to="/feedback" />
 				<van-cell title="Set NickName(निक नाम सेट करें)" is-link to="/setnickname" />
 				<van-cell title="Modify Head Portrait(अवतार सेट करें)" is-link to="/setavatar" />
+				<van-cell title="FAQs(सामान्य प्रश्न)" is-link to="/faqs" />
+				<van-cell title="About Us(हमारे बारे में)" is-link to="/aboutus" />
+			</div>
+			<div class="custom-cell-area">
+				<van-collapse v-model="activeNames">
+					<van-collapse-item title="Customer service(ग्राहक सेवा)" name="1">
+
+						<div type="button"
+							 v-clipboard:copy="message"
+							 v-clipboard:success="onCopy"
+							 v-clipboard:error="onError">
+							<p>Click to copy whatsapp: {{message}}</p>
+							<p>कॉपी पर क्लिक करें whatsapp: {{message}}</p>
+						</div>
+					</van-collapse-item>
+				</van-collapse>
 			</div>
 			<div class="custom-cell-area">
 				<van-cell title="Logout(लॉग आउट)" is-link @click="logout" />
@@ -31,7 +47,8 @@
 	export default {
 		data() {
 			return {
-
+				message: '+918939705079',
+				activeNames: ['1']
 			}
 		},
 		mounted() {
@@ -51,6 +68,12 @@
 			async logout() {
 				await this.$store.dispatch('user/logout')
 				this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+			},
+			onCopy (e) {
+				this.$toast.success('Copy successfully');
+			},
+			onError (e) {
+				this.$toast.fail('Copy Failed')
 			},
 		}
 	}
