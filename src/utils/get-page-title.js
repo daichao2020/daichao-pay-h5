05f0f4f289bcaa377c7bc1a438eeb93008290fd9
tshflyer@163.com <tshflyer@163.com>
2020-10-31 00:45:33 +0800

    indiloan 审核版

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Money Loan'
+const title = defaultSettings.title || 'Indi Loan'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
