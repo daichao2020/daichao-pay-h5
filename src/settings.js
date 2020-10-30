af97c892d8fa135058c3cbcf4b5255fecb4130cb
tshflyer@163.com <tshflyer@163.com>
2020-10-30 22:28:36 +0800

    indiloan

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	title				: 'Rupee Loan',
+	title				: 'Indi Loan',
 	deviceNumber		: '5566777',//设备号
 	platform			: '3',//平台：1-安卓 2-苹果 3-H5
 	appVersionId		: '48',//App 版本 id
--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Rupee Loan'
+const title = defaultSettings.title || 'Indi Loan'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
