3858eb5e237b19e0933c29b77e3a13bc753e90dd
tshflyer@163.com <tshflyer@163.com>
2020-10-31 21:01:36 +0800

    【cash loan】首页增加分类[打包]

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -14,20 +14,29 @@
 			<section class="home-page-body page-body">
 				<productSwiper :list="carouselList" v-if="carouselList.length>0"></productSwiper>
 
-				<div class="panel">
+				<div class="panel" v-for="category in categoryList">
 					<div class="panel-hd">
 						<div class="heading-item flex">
-							<van-icon class="icon" name="fire" />
+							<van-icon class="icon" name="fire"/>
 							<div class="heading-item-bd flex-1">
-								<p class="title">Personnal Loan</p>
-								<p class="desc">Unsecred Loan for persional purpose</p>
+								<p class="title">{{category.name}}</p>
+								<!--<p class="desc">Unsecred Loan for persional purpose</p>-->
 							</div>
 						</div>
 					</div>
 					<div class="panel-bd">
-						<van-skeleton title :loading="skeletonLoading" :row="3" />
+						<van-skeleton title :loading="skeletonLoading" :row="3"/>
 						<div class="product-list">
-							<productItem v-for="item in list" :key="item.id" :item="item"></productItem>
+							<productItem v-for="item in category.list" :key="item.id" :item="item"
+										 v-show="item.isShow"></productItem>
+						</div>
+					</div>
+					<div class="panel-ft" v-show="category.isShowViewMoreBtn">
+						<div class="text-center view-more-btn-wrap">
+							<van-button class="view-more-btn" size="small" plain icon="eye" block
+										@click="showMore(category)">
+								View More {{ category.name }}
+							</van-button>
 						</div>
 					</div>
 				</div>
@@ -39,51 +48,110 @@
 <script>
 
 	import productSwiper from '@/components/Public/swiper1'
-	import { getHomeProducts } from '@/api/product'
-	import { recordOP } from '@/api/user'
+	import {getHomeProducts} from '@/api/product'
+	import {recordOP} from '@/api/user'
 	import productItem from '@/components/Public/productItem.vue'
 
 	export default {
-		components:{
+		components: {
 			productSwiper,
 			productItem
 		},
-		data(){
+		data() {
 			return {
-				list: [],
+				categoryList: [],
 				carouselList: [],
 				isPullRefresh: false,
 				skeletonLoading: true,
 			}
 		},
-		created(){
+		created() {
 			this.getHomeProducts();
 		},
 		computed: {
 			isShowHeader() {
 				const route = this.$route
-				const { meta } = route
+				const {meta} = route
 				return meta.showHeader
 			},
 		},
 		methods: {
-			onRefresh(){
-				this.getHomeProducts().then(()=>{
+			onRefresh() {
+				this.getHomeProducts().then(() => {
 					this.isPullRefresh = false
 				})
 
 			},
-			getHomeProducts(){
-				return getHomeProducts().then(res=>{
+			getHomeProducts() {
+				return getHomeProducts().then(res => {
 					this.skeletonLoading = false;
-					const { data } = res;
-					this.list = data.special || [];
+					const {data} = res;
+					let list = data.special || [];
+					this.categoryList = this.filterListByCategory(list);
 					this.carouselList = data.carousel || [];
 
 				})
 			},
 			toPolicyPage(){//
 				this.$router.push({name:'policy'});
+			},
+			filterListByCategory(list) {
+
+				let categoryObj = {};
+				let emptyCategoryList = [];
+
+				list.forEach(item => {
+					if (item.category) {
+						if (!categoryObj[item.category]) {
+							categoryObj[item.category] = [];
+						}
+						categoryObj[item.category].push(item)
+					} else {
+						emptyCategoryList.push(item);
+					}
+				});
+
+				let resList = [];
+				for (var key in categoryObj) {
+
+					let tempList = categoryObj[key];
+					tempList.forEach((item, index) => {
+						if (index < 2) {
+							item.isShow = true;
+						} else {
+							item.isShow = false;
+						}
+					});
+
+					resList.push({
+						name: key,
+						isShowViewMoreBtn: tempList.length > 2 ? true : false,
+						list: tempList,
+					})
+				}
+				if (emptyCategoryList.length > 0) {
+					emptyCategoryList.forEach((item, index) => {
+						if (index < 2) {
+							item.isShow = true;
+						} else {
+							item.isShow = false;
+						}
+					});
+					resList.push({
+						name: 'Personnal Loan',
+						isShowViewMoreBtn: emptyCategoryList.length > 2 ? true : false,
+						list: emptyCategoryList,
+					})
+				}
+
+				return resList;
+			},
+			showMore(category) {
+				let list = category.list;
+				list.forEach(item => {
+					item.isShow = true;
+				});
+				category.isShowViewMoreBtn = false;
 			}
 		}
 	}
