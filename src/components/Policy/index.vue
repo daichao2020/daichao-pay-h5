7af4dc24409e86f61f4eaa30cc834dd5461905eb
tshflyer@163.com <tshflyer@163.com>
2020-11-02 21:26:09 +0800

    IndiLoan 隐私协议修改

--- src/components/Policy/index.vue
+++ src/components/Policy/index.vue
@@ -12,7 +12,128 @@
 		</header>
 
 		<section class="page-body">
-			<div class="WordSection1"><p class="MsoNormal"><span>Data Privacy Policy</span></p><p class="MsoNormal"><span>Please read this Privacy Policy carefully before accessing or using the Service. This Privacy Policy contains important terms that affect your legal rights. It is understood that upon submission of the application form, you agree to be bound by the terms and conditions set forth below and automatically consent to the collection, use, storage, analysis and manipulation of your data information in accordance with this Privacy Policy to provide you with a great customer experience. If you do not wish to be bound by these terms and conditions, you may not access or use money loan Service.</span></p><p class="MsoNormal"><span>Please note that this privacy policy may be amended from time to time to comply with Data Privacy requirements, coverage and technology security advancements and such modifications shall be effective immediately upon posting of the modified Agreement.</span></p><p class="MsoNormal"><span>Terms and Definitions</span></p><p class="MsoNormal"><span>Consent </span><span>–</span><span> Any freely given, specific, informed indication of will for the collection and processing of his or her personal, sensitive and privileged information.</span></p><p class="MsoNormal"><span>Company </span><span>–</span><span> refers to </span><span>“</span><span lang="EN-US">money loan</span><span>”</span><span lang="EN-US">.</span></p><p class="MsoNormal"><span>Data Outsourcing </span><span>–</span><span> disclosure or transfer of personal data by a personal information controller to a personal information processor.</span></p><p class="MsoNormal"><span>Data Sharing </span><span>–</span><span> Disclosure or transfer to a third party of personal data under the custody of a personal information controller or personal information processor. Disclosure or transfer of the personal information processor must be upon the instructions of the personal information controller.</span></p><p class="MsoNormal"><span>Data Subject - Refers to an individual whose personal, sensitive, and/or privileged information is processed.</span></p><p class="MsoNormal"><span>Personal Information </span><span>–</span><span> refers to all types of personal information which are apparent or can be reasonably and directly ascertained by the entity holding the information such as:</span></p><p class="MsoNormal"><span></span><span>Full Name;</span></p><p class="MsoNormal"><span></span><span>Photo;</span></p><p class="MsoNormal"><span></span><span>Signature;</span></p><p class="MsoNormal"><span></span><span>IP Address;</span></p><p class="MsoNormal"><span></span><span>SMS</span></p><p class="MsoNormal"><span></span><span>Contact</span></p><p class="MsoNormal"><span></span><span>Postpaid Number; </span></p><p class="MsoNormal"><span></span><span>Biometrics;</span></p><p class="MsoNormal"><span>Personal Information Controller (PIC) </span><span>–</span><span> refers to a natural or juridical person, or any other body who controls the processing of personal data, or instructs another to process personal data on its behalf</span></p><p class="MsoNormal"><span>Personal Information Processor (PIP) </span><span>–</span><span> refers to any natural or juridical person, or any other body who controls the processing of personal data, or instructs another to process personal data on its behalf</span></p><p class="MsoNormal"><span>Processing </span><span>–</span><span> refers to any operation or any set of operations performed upon personal data including, but not limited to collection, recording, organization, storage, updating or modification, retrieval, consultation, use, consolidation, blocking, erasure or destruction of data. Processing may be performed through automated means, or manual processing, if the personal data are contained or are intended to be contained in a filing system.</span></p><p class="MsoNormal"><span>Sensitive Personal Information </span><span>–</span><span> refers to personal information about the data subjects:</span></p><p class="MsoNormal"><span></span><span>Race;</span></p><p class="MsoNormal"><span></span><span>Ethnic Origin;</span></p><p class="MsoNormal"><span></span><span>Marital status;</span></p><p class="MsoNormal"><span></span><span>Age;</span></p><p class="MsoNormal"><span></span><span>Religious or Philosophical belief;</span></p><p class="MsoNormal"><span></span><span>Political Affiliation;</span></p><p class="MsoNormal"><span></span><span>Education;</span></p><p class="MsoNormal"><span></span><span>Genetic or sexual life;</span></p><p class="MsoNormal"><span></span><span>Any offense involving such person;</span></p><p class="MsoNormal"><span></span><span>Social Security Numbers;</span></p><p class="MsoNormal"><span></span><span>Licenses;</span></p><p class="MsoNormal"><span></span><span>Specifically Established by law to be kept classified</span></p><p class="MsoNormal"><span>Your Consent</span></p><p class="MsoNormal"><span>You hereby give your acknowledgement and consent to money loan to use your personal data for the aforesaid Purposes and Services. You agree that your consent will remain in place until your withdrawal by officially notifying the Company in writing.</span></p><p class="MsoNormal"><span>Personal Information and Sensitive Personal Information</span></p><p class="MsoNormal"><span>Personal and Sensitive Personal information are information that can be used to uniquely identify a user such as name, password, date of birth, civil status, gender, email address, telephone number, and other personal details. Below is a list of information that may be collected:</span></p><p class="MsoNormal"><span>•</span><span> Your SSS and/or TIN number;</span></p><p class="MsoNormal"><span>•</span><span> Your employment and income information;</span></p><p class="MsoNormal"><span>•</span><span> Your bank account information;</span></p><p class="MsoNormal"><span>•</span><span> Any personal information you provide us in connection with any customer service communication (email, phone or other means), dispute or complaint.</span></p><p class="MsoNormal"><span>You may be asked to provide Personal Information and/or Sensitive Personal Information to meet our legal obligations, verify your identity, determine eligibility for credit, protect against fraud, and complete your transaction.</span></p><p class="MsoNormal"><span>We may also use the provided Personal Information and Sensitive Personal Information to inform you of special loan offers, to process your loans, to provide you with our services or to conduct surveys. While the Company collects these information on a voluntary basis for certain products and services, The Company</span><span>’</span><span lang="EN-US">s collection of personally identifiable information may be a requirement for access to our services or to process a user's loan.</span></p><p class="MsoNormal"><span>How To Contact Us</span></p><p class="MsoNormal"><span>For any queries, clarifications or requests on any aspect of this Statement, the exercise of your rights pertaining to your personal information or to provide any feedback that you may have about our processing of personal information, you may get in touch with our Data Privacy Officer at:</span></p><p class="MsoNormal"><span>The Data Protection Officer</span></p><p class="MsoNormal"><span><a href="mailto:couplecartoon009@gmail.com">couplecartoon009@gmail.com</a></span></p><p class="MsoNormal"><span>Changes to Our Privacy Statement</span></p><p class="MsoNormal"><span>We may modify or amend this Privacy Statement from time to time to keep up with any changes in relevant laws and regulations applicable to us or how we collect, use, protect, store, share or dispose of your personal information. Any relevant updates will be posted on money loan website and mobile application.</span></p></div>
+
+			<div class=WordSection1 style='layout-grid:15.6pt'>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>indiloan Privacy Policy&nbsp;
+(&quot;us&quot;, &quot;we&quot;, or &quot;our&quot;) operates the indiloan
+mobile application (the &quot;Service&quot;).</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>This page informs you of our policies
+regarding the collection, use, and disclosure of personal data when you use our
+Service and the choices you have associated with that data.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>We use your data to provide and improve the
+Service. By using the Service, you agree to the collection and use of
+information in accordance with this policy. Unless otherwise defined in this
+Privacy Policy, terms used in this Privacy Policy have the same meanings as in
+our Terms and Conditions.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Information Collection And Use </span></p>
+
+				<p class=MsoNormal><span>We collect several different types of
+information for various purposes to provide and improve our Service to you.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Types of Data Collected Personal Data While
+using our Service, we may ask you to provide us with certain personally
+identifiable information that can be used to contact or identify you
+(&quot;Personal Data&quot;). Personally identifiable information may include,
+but is not limited to:</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>First name and last name Phone number
+Cookies and Usage Data</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Usage Data When you access the Service by
+or through a mobile device, we may collect certain information automatically,
+including, but not limited to, the type of mobile device you use, your mobile
+device unique ID, the IP address of your mobile device, your mobile operating
+system, the type of mobile Internet browser you use, unique device identifiers
+and other diagnostic data (&quot;Usage Data&quot;).</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Tracking &amp; Cookies Data </span></p>
+
+				<p class=MsoNormal><span>We use cookies and similar tracking
+technologies to track the activity on our Service and hold certain information.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Cookies are files with small amount of data
+which may include an anonymous unique identifier. Cookies are sent to your
+browser from a website and stored on your device. Tracking technologies also
+used are beacons, tags, and scripts to collect and track information and to
+improve and analyze our Service.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>You can instruct your browser to refuse all
+cookies or to indicate when a cookie is being sent. However, if you do not
+accept cookies, you may not be able to use some portions of our Service.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Security Of Data. </span></p>
+
+				<p class=MsoNormal><span>The security of your data is important to
+us, but easy loans app does not require or take any user data. All user data is
+stored locally on the users device. \n We shall not be responsible for any loss
+suffered by you should the Services be interfered with or be unavailable.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Service Providers. </span></p>
+
+				<p class=MsoNormal><span>We may employ third party companies and
+individuals to facilitate our Service (&quot;Service Providers&quot;), to
+provide the Service on our behalf, to perform Service-related services or to
+assist us in analyzing how our Service is used.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>These third parties have access to your
+Personal Data only to perform these tasks on our behalf and are obligated not
+to disclose or use it for any other purpose.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Analytics. </span></p>
+
+				<p class=MsoNormal><span>We may use third-party Service Providers to
+monitor and analyze the use of our Service.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>Google Analytics</span></p>
+
+				<p class=MsoNormal><span>Google Analytics is a web analytics service
+offered by Google that tracks and reports website traffic. Google uses the data
+collected to track and monitor the use of our Service. This data is shared with
+other Google services. Google may use the collected data to contextualize and
+personalize the ads of its own advertising network.</span></p>
+
+				<p class=MsoNormal><span>&nbsp;</span></p>
+
+				<p class=MsoNormal><span>For more information on the privacy practices
+of Google, please visit the Google Privacy &amp; Terms web page:
+https://policies.google.com/privacy?hl=en</span></p>
+
+			</div>
+
 		</section>
 	</div>
 </template>
